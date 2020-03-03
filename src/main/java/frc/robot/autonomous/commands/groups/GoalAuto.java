24c072792370ec927b61ac82417608adc7d8baf1
TheHardestBunnyAlive <alexsplisov@gmail.com>
2020-03-03 11:50:45 -0500

    Revert "Auto that needs to be tested"

--- src/main/java/frc/robot/autonomous/commands/groups/GoalAuto.java
+++ src/main/java/frc/robot/autonomous/commands/groups/GoalAuto.java
@@ -2,8 +2,10 @@ package frc.robot.autonomous.commands.groups;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.autonomous.commands.Aim;
-import frc.robot.autonomous.commands.DriveStraight;
+import frc.robot.autonomous.commands.BottomFeed;
 import frc.robot.autonomous.commands.RotateToAngle;
+import frc.robot.autonomous.commands.Shooter;
+import frc.robot.autonomous.commands.TopFeed;
 import frc.robot.autonomous.subsystems.BottomFeedSubsystem;
 import frc.robot.autonomous.subsystems.ShooterSubsystem;
 import frc.robot.autonomous.subsystems.TopFeedSubsystem;
@@ -16,7 +18,6 @@ public class GoalAuto extends SequentialCommandGroup {
 
     public GoalAuto(ShooterSubsystem shoot, TopFeedSubsystem topFeed, BottomFeedSubsystem bottomFeed) {
         addCommands(
-            new DriveStraight(0.5),
             new RotateToAngle(90),
             new Aim(),
             new Shoot());
