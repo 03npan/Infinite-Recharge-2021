babef8f240f2248c1b557a2bf052374608b600f2
Plisov <plisovas@fr-n-s081127.ncifcrf.gov>
2020-03-05 13:20:36 -0500

    Worked on main auto command and aiming command. Needs testing.

--- src/main/java/frc/robot/autonomous/commands/Aim.java
+++ src/main/java/frc/robot/autonomous/commands/Aim.java
@@ -1,23 +1,18 @@
 package frc.robot.autonomous.commands;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.utils.LimelightWrapper;
-import frc.robot.utils.Motors;
+import frc.robot.autonomous.subsystems.AimSubsystem;
 
 /**
  * Aim
  */
 public class Aim extends CommandBase {
 
-    private boolean m_LimelightHasValidTarget = false;
-    private double m_LimelightDriveCommand = 0.0;
-    private double m_LimelightSteerCommand = 0.0;
+    private final AimSubsystem m_aim;
 
-    public Aim() {
-        LimelightWrapper.ledMode(true);
+    public Aim(AimSubsystem m_aim) {
+        this.m_aim = m_aim;
+        addRequirements(m_aim);
     }
 
     @Override
@@ -27,94 +22,16 @@ public class Aim extends CommandBase {
 
     @Override
     public void execute() {
-        turnToTarget();
+        m_aim.turnToTarget();
     }
 
     @Override
     public void end(final boolean interrupted) {
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1);
-    }
-
-    private NetworkTable getTable() {
-        return NetworkTableInstance.getDefault().getTable("limelight");
+        m_aim.stopAiming();
     }
 
     @Override
     public boolean isFinished() {
-        return true;
+        return false;
     }
-
-    private void turnToTarget() {
-
-        Update_Limelight_Tracking();
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(3);
-        if (m_LimelightHasValidTarget) {
-            Motors.drive.arcadeDrive(m_LimelightDriveCommand, m_LimelightSteerCommand);
-        } else {
-            Motors.drive.arcadeDrive(0.0, 0.0);
-        }
-    }
-
-    public void Update_Limelight_Tracking() {
-        // These numbers must be tuned for your Robot! Be careful!
-        final double STEER_K = 0.03; // how hard to turn toward the target
-        final double DRIVE_K = 0.26; // how hard to drive fwd toward the target
-        final double DESIRED_TARGET_AREA = 13.0; // Area of the target when the robot reaches the wall
-        final double MAX_DRIVE = 0.5; // Simple speed limit so we don't drive too fast
-
-        final double x = LimelightWrapper.getX();
-        final double y = LimelightWrapper.getY();
-        final double area = LimelightWrapper.getArea();
-
-        if (!LimelightWrapper.isTargetAvalible()) {
-            m_LimelightHasValidTarget = false;
-            m_LimelightDriveCommand = 0.0;
-            m_LimelightSteerCommand = 0.0;
-            return;
-        }
-
-        m_LimelightHasValidTarget = true;
-
-        // Start with proportional steering
-        final double steer_cmd = x * STEER_K;
-        m_LimelightSteerCommand = steer_cmd;
-
-        // try to drive forward until the target area reaches our desired area
-        double drive_cmd = (DESIRED_TARGET_AREA - area) * DRIVE_K;
-
-        // don't let the robot drive too fast into the goal
-        if (drive_cmd > MAX_DRIVE) {
-            drive_cmd = MAX_DRIVE;
-        }
-        m_LimelightDriveCommand = drive_cmd;
-    }
-
-    private void aimAndRange() {
-        float KpAim = -0.1f;
-        float KpDistance = -0.1f;
-        float min_aim_command = 0.0f;
-
-        double tx = LimelightWrapper.getX();
-        double ty = LimelightWrapper.getY();
-
-        double heading_error = -tx;
-        double distance_error = -ty;
-        double steering_adjust = 0.0f;
-
-        if(tx > 1.0) {
-            steering_adjust = KpAim * heading_error - min_aim_command;
-        } else if(tx < 1.0) {
-            steering_adjust = KpAim * heading_error + min_aim_command;
-        }
-
-        double distance_adjust = KpDistance * distance_error;
-
-        double left_command = 0;
-        left_command += steering_adjust + distance_error;
-        double right_command = 0;
-        right_command -= steering_adjust + distance_adjust;
-
-        Motors.drive.tankDrive(left_command, right_command);
-    }
-
 }
--- src/main/java/frc/robot/utils/RobotContainer.java
+++ src/main/java/frc/robot/utils/RobotContainer.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.autonomous.commands.Aim;
 import frc.robot.autonomous.commands.ColorWheel;
 import frc.robot.autonomous.commands.groups.Shoot;
+import frc.robot.autonomous.subsystems.AimSubsystem;
 import frc.robot.autonomous.subsystems.BottomFeedSubsystem;
 import frc.robot.autonomous.subsystems.ColorWheelSubsystem;
 import frc.robot.autonomous.subsystems.DriveSubsystem;
@@ -26,6 +27,7 @@ public class RobotContainer {
     private final BottomFeedSubsystem m_bottomFeeder = new BottomFeedSubsystem();
     // private final ClimbSubsystem m_climb = new ClimbSubsystem();
     private final ColorWheelSubsystem m_colorWheel = new ColorWheelSubsystem();
+    private final AimSubsystem m_aim = new AimSubsystem();
 
     // Controllers
     public static XboxController dController = new XboxController(0);
@@ -40,9 +42,9 @@ public class RobotContainer {
 
     public RobotContainer() {
 
-        shootButton.whenActive(new Shoot());
-        aimButton.whenActive(new Aim());
-        spinColorWheelButton.whenActive(new ColorWheel(m_colorWheel));
+        shootButton.whenHeld(new Shoot());
+        aimButton.whenHeld(new Aim(m_aim));
+        spinColorWheelButton.whenHeld(new ColorWheel(m_colorWheel));
 
         // Default command set to drive
         m_robotDrive.setDefaultCommand(new RunCommand(() -> m_robotDrive.driveRobot(dController), m_robotDrive));
