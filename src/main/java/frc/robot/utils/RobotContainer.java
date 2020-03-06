0bf981ee70550ef5c2c5c86a0743a55c2873dc19
TheHardestBunnyAlive <alexsplisov@gmail.com>
2020-03-06 11:21:34 -0500

    Updated most of the commands to work with the 2020 robot. Controls still need to be tuned.

--- src/main/java/frc/robot/utils/RobotContainer.java
+++ src/main/java/frc/robot/utils/RobotContainer.java
@@ -4,13 +4,16 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.autonomous.commands.Aim;
+import frc.robot.autonomous.commands.BottomFeed;
 import frc.robot.autonomous.commands.ColorWheel;
-import frc.robot.autonomous.commands.groups.Shoot;
+import frc.robot.autonomous.commands.Intake;
+import frc.robot.autonomous.commands.Shooter;
+import frc.robot.autonomous.commands.TopFeed;
 import frc.robot.autonomous.subsystems.AimSubsystem;
 import frc.robot.autonomous.subsystems.BottomFeedSubsystem;
 import frc.robot.autonomous.subsystems.ColorWheelSubsystem;
 import frc.robot.autonomous.subsystems.DriveSubsystem;
+import frc.robot.autonomous.subsystems.IntakeSubsystem;
 import frc.robot.autonomous.subsystems.ShooterSubsystem;
 import frc.robot.autonomous.subsystems.TopFeedSubsystem;
 
@@ -21,7 +24,7 @@ public class RobotContainer {
 
     // The robot's subsystems
     private final DriveSubsystem m_robotDrive = new DriveSubsystem();
-    // private final IntakeSubsystem m_intake = new IntakeSubsystem();
+    private final IntakeSubsystem m_intake = new IntakeSubsystem();
     private final ShooterSubsystem m_shooter = new ShooterSubsystem();
     private final TopFeedSubsystem m_topFeeder = new TopFeedSubsystem();
     private final BottomFeedSubsystem m_bottomFeeder = new BottomFeedSubsystem();
@@ -37,14 +40,18 @@ public class RobotContainer {
     private final Command m_autoCommand = null; // ADD
 
     final JoystickButton shootButton = new JoystickButton(dController, XboxController.Button.kA.value);
-    final JoystickButton aimButton = new JoystickButton(dController, XboxController.Button.kX.value);
-    final JoystickButton spinColorWheelButton = new JoystickButton(dController, XboxController.Button.kB.value);
+    final JoystickButton bottomFeederButton = new JoystickButton(dController, XboxController.Button.kX.value);
+    final JoystickButton topFeederButton = new JoystickButton(dController, XboxController.Button.kB.value);
+    final JoystickButton intakeButton = new JoystickButton(dController, XboxController.Button.kY.value);
+    final JoystickButton colorWheelButton = new JoystickButton(dController, XboxController.Button.kBumperLeft.value);
 
     public RobotContainer() {
 
-        shootButton.whenHeld(new Shoot());
-        aimButton.whenHeld(new Aim(m_aim));
-        spinColorWheelButton.whenHeld(new ColorWheel(m_colorWheel));
+        shootButton.whenHeld(new Shooter(m_shooter));
+        bottomFeederButton.whenHeld(new BottomFeed(m_bottomFeeder));
+        topFeederButton.whenHeld(new TopFeed(m_topFeeder));
+        intakeButton.whenHeld(new Intake(m_intake));
+        colorWheelButton.whenHeld(new ColorWheel(m_colorWheel));
 
         // Default command set to drive
         m_robotDrive.setDefaultCommand(new RunCommand(() -> m_robotDrive.driveRobot(dController), m_robotDrive));
